2cad1262b2888d4bc8791fc0b5d23f313a33bd68
Nori <norizon16@proton.me>
2026-04-09 10:56:05 +0700

    fix: allow files with no extensions

--- src/assets.rs
+++ src/assets.rs
@@ -1,11 +1,14 @@
-use std::{error::Error, path::Path};
+use std::path::Path;
 
 /// This function return the corresponding asset id which is the name of the file in the discord developper portal
 /// We match first on the filename if it is known it is returned, else we match on the extension.
 /// If the extension is unknown then it falls back to helix icon
 /// Note that you are not able to know in advance the asset id, only @ciflire can, so if you have any remark please open an issue
 pub fn get_asset(filename: String) -> String {
-    let extension = Path::new(&filename).extension().unwrap().to_str().unwrap();
+    let extension = Path::new(&filename)
+        .extension()
+        .and_then(|e| e.to_str())
+        .unwrap_or_default();
     let res = match filename.as_str() {
         "Cargo.toml" | "Cargo.lock" => "cargo".to_string(),
         "hyprland.conf" | "hyprlock.conf" | "hyprpaper.conf" | "hypridle.conf" => {
